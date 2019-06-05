23b6c4c8ca4879184670f2a3271f7bcf5691d0ee
Peter Nikopoulos <peter@nikopoulos.net>
2019-06-04 23:56:11 -0400

    Began serial port reading function to send commands to stepper motors

--- stepperdrivers.ino
+++ stepperdrivers.ino
@@ -18,7 +18,7 @@
 //Pretty much useless for this application anyways, 200 steps per rev is a high enough resolution for the etch a sketch
 #define Microsteps 1
 
-//Microstepping Pins (still useless)
+//Microstepping Pins (still useless) -- because at smaller steps the motors dont have enough torque to turn the nobs, plus the etch a sketch doesnt have a high resolution anyways
 #define MS1 4
 #define MS2 3
 #define MS3 2
@@ -42,13 +42,32 @@ SyncDriver controller(Stepper_X, Stepper_Y);
 void setup(){
     Stepper_X.begin(RPM_X);
     Stepper_Y.begin(RPM_Y);
-
+    
+    Serial.begin(9600);
+    delay(1000);
+    Serial.print("test");
 }
 
+int X_move = 0;
+int Y_move = 0;
+input == " ";
 
+void grabSerial(){
+    if(Serial.available()){
+        input = String(Serial.read());
+        input.substring(0,4) = X_move;
+        input.substring(5,10) = Y_move;
+        
+    }
+    return X_move , Y_move;
+}
 
 void loop(){
-    controller.move(200, -200); // x , y
+    grabSerial();
+    Serial.print(X_move);
+    Serial.print(Y_move);
+    
+//    controller.move(200, -200); // x , y
 
     delay(1000);
 }
